4c14008f0c14fa1e69183b00ce423111a70a4e91
beadsland <beads.d.land@gmail.com>
2019-12-25 21:10:57 -0500

    case-insensitive search of paths

---
+++ searchLocations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+find = "An Alexander Technique Student talks 1"
+
+import re
+import couchdb
+import sys
+
+couch = couchdb.Server("http://192.168.2.52:4000")
+couch.resource.credentials = ("itunes", "senuti")
+db = couch['audio_library']
+n = 0
+
+for id in db:
+  n = n + 1
+  pref = db[id]['iTunes']['iTunes Library']['Music Folder']
+  path = db[id]['iTunes']['Location'].replace(pref, "")
+  sys.stdout.write("> %d: %s     \r" % (n, path[:100]))
+
+  if re.search(find, path, re.IGNORECASE):
+    print("\n%s" % path)
