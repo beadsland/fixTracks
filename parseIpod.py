f22540f7d19a64c30cee473c3ab08e6069cdd6ad
beadsland <beads.d.land@gmail.com>
2019-12-31 07:55:54 -0500

    sort by datetime released

--- parseIpod.py
+++ parseIpod.py
@@ -2,6 +2,7 @@
 
 # apt install python-gpod; unrelated to pip3 install gpod
 import gpod
+import datetime
 
 path = "/media/removable/IEUSER'S IP"
 
@@ -10,5 +11,15 @@ db = gpod.Database(path, None)
 #db = gpod.Database("/media/BEADS\'S\ IPO")
 #gpod.itdb_device_set_sysinfo (db._itdb.device, "ModelNumStr", "xC293");
 
-for track in db:
-    print hex(track['dbid']), track
+#for track in db:
+#    print hex(track['dbid']), track['time_released'], track
+
+def time_released(track):
+  try:
+    return track['time_released']
+  except:
+    return datetime.datetime.min
+
+forward = sorted(db, key = lambda track: time_released(track))
+
+print(forward[:20])
