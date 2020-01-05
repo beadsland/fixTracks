3e15338c813c18fd96e1437c566d3182f4687ef7
beadsland <beads.d.land@gmail.com>
2020-01-04 21:19:59 -0500

    preliminary prototyping of auto-ejecting the ipod

--- parseIpod.py
+++ parseIpod.py
@@ -1,5 +1,29 @@
 #!/usr/bin/env python
 
+import psutil
+mounts = {p.mountpoint: p for p in psutil.disk_partitions()}
+mount = mounts['/media/beads/IEUSER\'S IP'].device.replace('/dev/', '')
+
+import blkinfo
+devices = [(d['children'], d) for d in blkinfo.BlkDiskInfo().get_disks()]
+devices = [map(lambda x: (x, dct), lst) for (lst, dct) in devices]
+devices = [item for sublist in devices for item in sublist]
+devices = {x.encode('UTF-8'): y for (x, y) in devices}
+
+import usb.core
+usbdevs = {dev.serial_number: dev for dev in usb.core.find(find_all = True)}
+usbdev = usbdevs[devices[mount]['serial']]
+#usbports = '.'.join([str(n) for n in usbdev.port_numbers])
+#usbdev = "%s-%s" % (usbdev.bus, usbports)
+usbdev.detach_kernel_driver(0)
+
+
+
+
+
+exit()
+
+
 import savvy
 db = savvy.init()
 
