78b3fdde614d5feb4faafb2a4d89afe213aad7ec
beadsland <beads.d.land@gmail.com>
2020-01-10 06:37:26 -0500

    traverse remaining files

--- scanUntrackedFiles.py
+++ scanUntrackedFiles.py
@@ -29,6 +29,29 @@ class Folder:
       prep = "Folder(\"%s\")" % self.path.replace(root, "%_ITUNES")
       return "<dict size %d> + %s" % (len(self.dict), prep)
 
+  def __iter__(self):
+    return self
+
+  def __next__(self):
+    if not hasattr(self, 'iter'): self.iter = os.scandir(self.path)
+    while self.iter:
+      while len(self.dict):
+        key = list(self.dict.keys())[0]
+        result = self._next_deep(key)
+        if result: return os.path.join(self.path, result)
+        self.dict.pop(key)
+      self._get_push(next(self.iter))
+
+  def _next_deep(self, key):
+    if type(self.dict[key]) is str:
+      return self.dict.pop(key)
+    else:
+      folder = self.dict[key]
+      try:
+        return os.path.join(key, next(folder))
+      except:
+        return None
+
   def get(self, key):
     if key in self.dict: # only retain leaves until they've been gotten
       if type(self.dict[key]) is str: return self.dict.pop(key)
@@ -37,46 +60,57 @@ class Folder:
 
     while not key in self.dict and self.iter is not None:
       sys.stdout.write("> %d in %s\r" % (len(self.dict), self.path))
-      n = next(self.iter, None)
-      if n is None:
-        self.iter = None
-      else:
-        if n.is_dir():
-          self.dict[n.name] = Folder(n.path)
-        else:
-          self.dict[n.name] = n.path
+      self._get_push(next(self.iter))
 
     if key in self.dict: return self.dict[key]
     raise Exception('Path Not Cached: %s' % os.path.join(self.path, key))
 
+  def _get_push(self, n):
+    if n.is_dir():
+      self.dict[n.name] = Folder(n.path)
+    else:
+      self.dict[n.name] = n.path
 
-print("Assembling paths...")
-dirs = ["Downloads", "iPod only", "Broadcast", "Music"]
-dirs = dirs + ["Podcasts%s" % (" Over"*i) for i in range(0, 4)]
-dirs = {f: Folder(os.path.join(root, "iTunes Media", f)) for f in dirs}
-dirs["iTunes U"] = dirs["Podcasts"]  # This one's a softlink
-dirs = {"Podcasts": Folder(os.path.join(root, "Podcasts")),
-        "iTunes Media": Folder(os.path.join(root, "iTunes Media"), dirs)}
+def assemble_paths():
+  print("Assembling paths...")
+  dirs = ["Downloads", "iPod only", "Broadcast", "Music"]
+  dirs = dirs + ["Podcasts%s" % (" Over"*i) for i in range(0, 4)]
+  dirs = {f: Folder(os.path.join(root, "iTunes Media", f)) for f in dirs}
+  dirs["iTunes U"] = dirs["Podcasts"]  # This one's a softlink
+  dirs = {"Podcasts": Folder(os.path.join(root, "Podcasts")),
+          "iTunes Media": Folder(os.path.join(root, "iTunes Media"), dirs)}
+  return dirs
 
-print(dirs)
+def match_tracks(dirs):
+  print("\nAccessing database...")
+  couch = couchdb.Server("http://192.168.2.52:4000")
+  couch.resource.credentials = ("itunes", "senuti")
+  db = couch['audio_library']
+  for id in db:
+    if db[id]['iTunes']['Track Type'] == "URL": continue
+    if db[id]['iTunes']['Location'].startswith("file://localhost/N:/Torrents/"): continue
 
-print("\nAccessing database...")
-couch = couchdb.Server("http://192.168.2.52:4000")
-couch.resource.credentials = ("itunes", "senuti")
-db = couch['audio_library']
-for id in db:
-  if db[id]['iTunes']['Track Type'] == "URL": continue
-  if db[id]['iTunes']['Location'].startswith("file://localhost/N:/Torrents/"): continue
+    pref = db[id]['iTunes']['iTunes Library']['Music Folder']
+    path = db[id]['iTunes']['Location'].replace(pref, "")
+    print("\nSeeking %s: %s..." % (id, path))
+    path = path.split('/')
+    print(path)
 
-  pref = db[id]['iTunes']['iTunes Library']['Music Folder']
-  path = db[id]['iTunes']['Location'].replace(pref, "")
-  print("\nSeeking %s: %s..." % (id, path))
-  path = path.split('/')
-  print(path)
+    folder = dirs
+    for p in path:
+      folder = folder.get(p)
+      print("⇒ %s: %s" % (p, folder))
 
-  folder = dirs
-  for p in path:
-    folder = folder.get(p)
-    print("⇒ %s: %s" % (p, folder))
+dirs = assemble_paths()
+match_tracks(dirs)
+
+print("Traverse remaining files...")
+i = 0
+for d in dirs:
+  for f in dirs[d]:
+    i += 1
+    print(i, f)
+
+
 
 def get_ext(f): return os.path.splitext(f)[1]
