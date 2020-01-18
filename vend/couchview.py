2bb7398c20ae5dbb8265291d5bd2e3c2675d675f
beadsland <beads.d.land@gmail.com>
2020-01-18 17:49:58 -0500

    rudimentary view functionality using python

---
+++ vend/couchview.py
@@ -0,0 +1,48 @@
+###
+# Source: https://markhaa.se/couchdb-views-in-python.html
+#
+# Copyright 2012 Mark Haase. Used in good faith.
+###
+
+# To use, add the following to /opt/couchdb/bin/couchdb
+#
+#  COUCHDB_QUERY_SERVER_PYTHON = "/usr/local/bin/couchpy"
+#
+# Per: https://docs.couchdb.org/en/stable/config/query-servers.html
+# Per: https://couchdb-python.readthedocs.io/en/latest/views.html
+
+from couchdb.design import ViewDefinition
+import inflection
+import sys
+
+class CouchView(ViewDefinition):
+    """
+    A base class for couch views that handles the magic of instantiation.
+    """
+
+    def __init__(self):
+        """
+        Does some magic to map the subclass implementation into the format
+        expected by ViewDefinition.
+        """
+
+        module = sys.modules[self.__module__]
+        design_name = module.__name__.split('.')[-1]
+
+        if hasattr(self.__class__, "map"):
+            map_fun = self.__class__.map
+        else:
+            raise NotImplementedError("Couch views require a map() method.")
+
+        if hasattr(self.__class__, "reduce"):
+            reduce_fun = self.__class__.reduce
+        else:
+            reduce_fun = None
+
+        super_args = (design_name,
+                      inflection.underscore(self.__class__.__name__),
+                      map_fun,
+                      reduce_fun,
+                      'python')
+
+        super(CouchView, self).__init__(*super_args)
